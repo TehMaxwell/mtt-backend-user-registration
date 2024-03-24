142b3d7565967628190951dcf3b511fc76c28701
Sam Maxwell <maxwell.g.sam@gmail.com>
2024-03-24 22:47:51 +0000

    Added Producer, Helm Deployment
    - Added code for producing a message in the Kafka Consumer.
    - Started working on Helm Deployment charts.

--- user-registration-requested/Consumer.cs
+++ user-registration-requested/Consumer.cs
@@ -5,8 +5,10 @@ DESCRIPTION: The Kafka consumer class for the user-registration-requested consum
 */
 
 using Confluent.Kafka;
+using Microsoft.Extensions.Options;
 
-namespace MTT.UserRegistrationRequested {
+namespace MTT.UserRegistrationRequested
+{
     /// <summary>
     /// The Kafka consumer class for the user-registration-requested consumer.
     /// </summary>
@@ -14,48 +16,77 @@ namespace MTT.UserRegistrationRequested {
     {
         // PROPERTIES
         string consumerTopic = "user-registration-requested";
-        Guid consumerGuid = Guid.NewGuid();
+        string producerTopic = "user-registration-completed";
+        Guid containerGuid = Guid.NewGuid();
 
         // Kafka Consumer Configuration
-        ConsumerConfig consumerConfig = new ConsumerConfig
-        {
-            BootstrapServers = "Kafka.Kafka:9092",
-            GroupId = "user-registration-requested-group",
-            AutoOffsetReset = AutoOffsetReset.Earliest,
-            EnableAutoCommit = false        // Disables auto commiting, allowing for "at-least once" functionality in the consumer
-        };
+        // ConsumerConfig consumerConfig = new ConsumerConfig
+        // {
+        //     BootstrapServers = "Kafka.Kafka:9092",
+        //     GroupId = "user-registration-requested-group",
+        //     AutoOffsetReset = AutoOffsetReset.Earliest,
+        //     EnableAutoCommit = false        // Disables auto commiting, allowing for "at-least once" functionality in the consumer
+        // };
+        ConsumerConfig consumerConfig;
+
+        // Kafka Producer Configuration
+        // ProducerConfig producerConfig = new ProducerConfig 
+        // {
+        //     BootstrapServers = "Kafka.Kafka:9092"
+        // };
+        ProducerConfig producerConfig;
 
         // METHODS
+        /// <summary>
+        /// Constructor. Performs dependency injection activities.
+        /// </summary>
+        public UserRegistrationRequestedConsumer(IOptions<ConsumerConfig> consumerConfigContainer, IOptions<ProducerConfig> producerConfigContainer) {
+            consumerConfig = consumerConfigContainer.Value;
+            producerConfig = producerConfigContainer.Value;
+        }
+
         /// <summary>
         /// Starts the Kafka consumption loop and executes the relevant code each time a message is available.
         /// </summary>
         public async Task Start() {
             // Building the consumer with the "using" structure. Ensures correct disposal of consumer resources at the end of execution.
             using (IConsumer<Ignore, string> consumer = new ConsumerBuilder<Ignore, string>(consumerConfig).Build()) {
-                consumer.Subscribe(consumerTopic);      // Subscribing to the configured topic
-                CancellationToken cancellationToken = new CancellationToken();      // Creating the cancellation token for the consumer
+                using (IProducer<Null, string> producer = new ProducerBuilder<Null, string>(producerConfig).Build()) {
+                    consumer.Subscribe(consumerTopic);      // Subscribing to the configured topic
+                    CancellationToken cancellationToken = new CancellationToken();      // Creating the cancellation token for the consumer
 
-                while(!cancellationToken.IsCancellationRequested) {
-                    ConsumeResult<Ignore, string> consumeResult = consumer.Consume(cancellationToken);
+                    Console.WriteLine($"Consumer with GUID {containerGuid} has subscribed to topic {consumerTopic}");
 
-                    // HANDLE THE CONSUMED MESSAGE BETWEEN THESE COMMENTS
-                    
-                    Console.WriteLine($"Consumer: {consumerGuid}, Message received: {consumeResult.Message}");
+                    while(!cancellationToken.IsCancellationRequested) {
+                        ConsumeResult<Ignore, string> consumeResult = consumer.Consume(cancellationToken);
 
-                    // HANDLE THE CONSUMED MESSAGE BETWEEN THESE COMMENTS
+                        // HANDLE THE CONSUMED MESSAGE BETWEEN THESE COMMENTS
+                        
+                        Console.WriteLine($"Consumer: {containerGuid}, Message received: {consumeResult.Message}");
+                        await Task.Delay(3000);
 
-                    // Commiting the new offset to the Kafka Topic, enables the at-least once processing behaviour
-                    try {
-                        consumer.Commit(consumeResult);
+                        Message<Null, string> produceMessage = new Message<Null, string> {
+                            Value = $"Message from Container {containerGuid}: hello there!"
+                        };
+                        await producer.ProduceAsync(producerTopic, produceMessage);
+
+                        // HANDLE THE CONSUMED MESSAGE BETWEEN THESE COMMENTS
+
+                        // Commiting the new offset to the Kafka Topic, enables the at-least once processing behaviour
+                        try {
+                            consumer.Commit(consumeResult);
+                        }
+                        
+                        catch (KafkaException exception) {
+                            Console.WriteLine($"Commit error: {exception.Error.Reason}");
+                        }
                     }
-                    
-                    catch (KafkaException exception) {
-                        Console.WriteLine($"Commit error: {exception.Error.Reason}");
-                    }
+
+                    consumer.Close();
                 }
-
-                consumer.Close();
             }
+
+            return;
         }
     }
 }
--- user-registration-requested/Program.cs
+++ user-registration-requested/Program.cs
@@ -4,13 +4,27 @@ DATE CREATED: 23/03/2024
 DESCRIPTION: Top level execution file for the user-registration-requested-consumer.
 */
 
+using Confluent.Kafka;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MTT.UserRegistrationRequested;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-// ADD SERVICE REGISTRATION CODE HERE
+// ADD SERVICE REGISTRATION CODE BETWEEN COMMENTS
+
+// Registration of Configuration Services
+builder.Services.Configure<ConsumerConfig>(builder.Configuration.GetSection("ConsumerConfig"));
+builder.Services.Configure<ProducerConfig>(builder.Configuration.GetSection("ProducerConfig"));
+
+// Registration of Kafka Services
+builder.Services.AddScoped<UserRegistrationRequestedConsumer>();
+
+// ADD SERVICE REGISTRATION CODE BETWEEN COMMENTS
 
 IHost host = builder.Build();
+
+UserRegistrationRequestedConsumer consumer = host.Services.GetRequiredService<UserRegistrationRequestedConsumer>();
+Task consumerTask = consumer.Start();
 
 await host.RunAsync();      // Running the application code
